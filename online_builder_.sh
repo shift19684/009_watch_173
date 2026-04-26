2ffb4029a2c7919bb55f6f2d7e0ae05a6c049c40
shift19684 <Srikomala_Social_Media@outlook.com>
2026-04-26 20:55:13 +0900

    Remove sudo from online_builder_.sh commands

--- online_builder_.sh
+++ online_builder_.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-sudo echo "online builder turned"
+echo "online builder turned"
 
-sudo python3 -m venv ./.bask/.myenv
-sudo source ./.bask/.myenv/bin/activate
+python3 -m venv ./.bask/.myenv
+source ./.bask/.myenv/bin/activate
 pip install --upgrade pip
 pip install awsiotsdk==1.27.0 awscrt==0.30.0
 pip install -U pyarmor
